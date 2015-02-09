2531a7fcb4d11fb12013bdeb7b18509f39b9c1ae
Marcus Utter <originalutter@gmail.com>
2015-02-09 14:33:01 +0100

    Removed projects and added test library

---
+++ UnitTests/Tests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void should_be_able_to_create_an_instance_of_dog()
+        {
+            Assert.That(true, Is.EqualTo(true));
+        }
+    }
+}
