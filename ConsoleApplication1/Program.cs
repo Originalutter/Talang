b5b9a7f6b913a242a3442e698599662c7749e755
Marcus Utter <originalutter@gmail.com>
2015-02-09 13:50:35 +0100

    Test

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new MyDatabaseDataContext())
+            {
+                var result = from c in db.Customers select c;
+                foreach (var c in result)
+                {
+                    c.Address = "Linq2Sql";
+                    c.Updated = DateTime.Now;
+                }
+                db.SubmitChanges();
+            }
+
+            using (var db2 = new MyDBEntities())
+            {
+                var result = from c in db2.Customers select c;
+                foreach (var c in result)
+                {
+                    c.Address = "Entity";
+                    c.Updated = DateTime.Now;
+                }
+                db2.SaveChanges();
+            }
+        }
+    }
+}
---
+++ MyLibrary/HelloWorld.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyLibrary
+{
+    public static class HelloWorld
+    {
+        public static void WriteHello()
+        {
+            Console.WriteLine(value: "Hello World!");
+        }
+    }
+}
